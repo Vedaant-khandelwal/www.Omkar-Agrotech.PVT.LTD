a7e45e664a64affbcab01f931cee12918430458f
Vedaant-khandelwal <74973418+Vedaant-khandelwal@users.noreply.github.com>
2021-01-07 10:27:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function onclick(1)
+
+{
+      onclick= document.getElementById(join);
+    window.alert(phone.no=9422806350);  
+}
+function onclick(2)
+{
+    onclick= document.getElementById(sell);
+    window.alert(phone.no=9422806350);'
+}
+function onclick(3)
+{
+    onclick= document.getElementById(buy;
+        window.alert(phone.no=9422806350); 
+}
